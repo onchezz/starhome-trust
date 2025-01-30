5fbc3f9d335976275a036599397ac9466f18e1a2
onchezz <brianonchezz@gmail.com>
2025-01-30 13:56:47 +0300

    changed basic info for properties

--- src/components/property/form/BasicInformation.tsx
+++ src/components/property/form/BasicInformation.tsx
@@ -3,8 +3,9 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Property } from "@/types/property";
-import { propertyTypes, statusOptions, tokenOptions } from "@/utils/constants";
+import { Property, propertyTypes, statusOptions } from "@/types/property";
+import { tokenOptions } from "@/utils/constants";
+
 
 interface BasicInformationProps {
   formData: Partial<Property>;
